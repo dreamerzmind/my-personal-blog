531ec2e288b951f3d61043d09b362bdc2a478cfd
narguis <narguis.guilherme@gmail.com>
2025-07-14 22:44:13 -0300

    Transferred files into a full next project

---
+++ pages/blog/FirstBlog.tsx
@@ -0,0 +1,11 @@
+
+
+const BlogPost = {
+    id: '1',
+    title: 'Creating My Personal Blog',
+    date: 'July 1, 2025',
+    author: 'Manal Ibrahim',
+    content:'<p>This is a simple blog post.</p>'
+    tags: [ 'typescript', 'react', 'blog' ]
+}
+    
